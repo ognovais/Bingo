27053b269c7598e0fdc3bfe6ff6be97cd84687d7
ognovais <159445979+ognovais@users.noreply.github.com>
2024-05-29 10:52:30 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,35 +4,79 @@ function gerarCartela() {
     cartela.innerHTML = '';
 
     var cabecalho = document.createElement('tr');
-    
+
     var nomeCelulaLabel = document.createElement('td');
     nomeCelulaLabel.textContent = 'Nome: ';
     cabecalho.appendChild(nomeCelulaLabel);
-    
+
     var nomeCelulaValor = document.createElement('td');
     nomeCelulaValor.textContent = nome;
     nomeCelulaValor.colSpan = 4;
     cabecalho.appendChild(nomeCelulaValor);
-    
+
     cartela.appendChild(cabecalho);
 
-    var numerosCartela = [];
-    while (numerosCartela.length < 25) {
-        var numero = Math.floor(Math.random() * 99) + 1;
+    var letras = ['B', 'I', 'N', 'G', 'O'];
+    var letrasCabecalho = document.createElement('tr');
+    letras.forEach(function(letra) {
+        var letraCelula = document.createElement('td');
+        letraCelula.textContent = letra;
+        letrasCabecalho.appendChild(letraCelula);
+    });
 
-        if (!numerosCartela.includes(numero)) {
-            numerosCartela.push(numero);
+    cartela.appendChild(letrasCabecalho);
+
+    var numerosCartela = {
+        'B': [],
+        'I': [],
+        'N': [],
+        'G': [],
+        'O': []
+    };
+
+    while (numerosCartela['B'].length < 5) {
+        var numero = Math.floor(Math.random() * 15) + 1;
+        if (!numerosCartela['B'].includes(numero)) {
+            numerosCartela['B'].push(numero);
+        }
+    }
+
+    while (numerosCartela['I'].length < 5) {
+        var numero = Math.floor(Math.random() * 15) + 16;
+        if (!numerosCartela['I'].includes(numero)) {
+            numerosCartela['I'].push(numero);
+        }
+    }
+
+    while (numerosCartela['N'].length < 5) {
+        var numero = Math.floor(Math.random() * 15) + 31;
+        if (!numerosCartela['N'].includes(numero)) {
+            numerosCartela['N'].push(numero);
+        }
+    }
+
+    while (numerosCartela['G'].length < 5) {
+        var numero = Math.floor(Math.random() * 15) + 46;
+        if (!numerosCartela['G'].includes(numero)) {
+            numerosCartela['G'].push(numero);
+        }
+    }
+
+    while (numerosCartela['O'].length < 5) {
+        var numero = Math.floor(Math.random() * 15) + 61;
+        if (!numerosCartela['O'].includes(numero)) {
+            numerosCartela['O'].push(numero);
         }
     }
 
     for (var i = 0; i < 5; i++) {
         var linha = document.createElement('tr');
 
-        for (var j = 0; j < 5; j++) {
+        ['B', 'I', 'N', 'G', 'O'].forEach(function(letra) {
             var coluna = document.createElement('td');
-            coluna.textContent = numerosCartela[i * 5 + j];
+            coluna.textContent = numerosCartela[letra][i];
             linha.appendChild(coluna);
-        }
+        });
 
         cartela.appendChild(linha);
     }
